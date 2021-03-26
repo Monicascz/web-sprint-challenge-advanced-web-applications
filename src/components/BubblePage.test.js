9fa3e195a8e676ca4128d573d9581b849a3888e0
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-26 15:56:20 -0700

    completed tests

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,13 +1,46 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+
+import mockGetBubbles from './../../src/mocks/handlers'
+jest.mock('./../../src/mocks/handlers');
+
+let testColors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  }
+]
 
 test("Renders BubblePage without errors", () => {
   // Finish this test
+  render(<BubblePage/>)
+
 });
 
-test("Fetches data and renders the bubbles on mounting", () => {
+test("Fetches data and renders the bubbles on mounting", async() => {
   // Finish this test
+  mockGetBubbles.mockResolvedValueOnce(testColors)
+  render(<BubblePage/>)
+  
+
+  await waitFor(()=>{
+    const colors = screen.queryAllByTestId("color")
+    expect(colors).toHaveLength(11)
+})
+
+
+
 });
 
 //Task List
